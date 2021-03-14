7f640a6219ca4991eafde1c52df00050cb5743e6
Zhenyu Ye <673598755@qq.com>
2021-03-14 04:26:54 -0700

    add Servlet

--- src/main/java/com/zhenyuye/webapp/WebappApplication.java
+++ src/main/java/com/zhenyuye/webapp/WebappApplication.java
@@ -3,14 +3,20 @@ package com.zhenyuye.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class WebappApplication {
+public class WebappApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebappApplication.class);
+    }
 }
