cdbfcb1af5ac2bcc960c657ca4c6f21f8d327039
Zhenyu Ye <673598755@qq.com>
2021-03-14 17:55:34 -0700

    update afterInstall script.
    request-checks:true

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,8 +2,4 @@
 sudo systemctl stop tomcat.service
 
 sudo chown tomcat:tomcat /opt/tomcat/latest/webapps/*.war
-## cleanup log files
-#sudo rm -rf /opt/tomcat/logs/catalina*
-#sudo rm -rf /opt/tomcat/logs/*.log
-#sudo rm -rf /opt/tomcat/logs/*.txt
 sudo systemctl start tomcat.service
